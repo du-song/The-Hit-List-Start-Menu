a67921b7ff532f2bc69a7545f5d65129e462e1c6
Du Song <sdu@freewheel.tv>
2015-01-26 03:00:36 -0500

    Initial Commit

---
+++ The Hit List Start Menu/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  The Hit List Start Menu
+//
+//  Created by Du Song on 2015-01-26.
+//  Copyright (c) 2015 Song Du. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ The Hit List Start MenuTests/The_Hit_List_Start_MenuTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Hit_List_Start_MenuTests.swift
+//  The Hit List Start MenuTests
+//
+//  Created by Du Song on 2015-01-26.
+//  Copyright (c) 2015 Song Du. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class The_Hit_List_Start_MenuTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
